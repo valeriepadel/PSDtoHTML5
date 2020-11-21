f0c83ea3edee10cc84833afe4fb8e20e2011eb38
valeriepadel <padelvalerie2307@gmail.com>
2020-11-21 22:53:48 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready (function() {
+
+	new WOW().init();
+
+   $('.js-wp-2').waypoint(function(direction) {
+       
+       $('.js-wp-2').addClass('animate__animated animate__slideInUp');
+   }, {
+		offset: '70%'
+	});
+
+   $('.js-wp-3').waypoint(function(direction) {
+       
+       $('.js-wp-3').addClass('animate__animated animate__fadeIn');
+   }, {
+		offset: '70%'
+	});
+
+ 		 $(".iphone-btn").delay(2300).animate( {bottom:"+=-3"}, 300);
+ 		 $(".iphone-btn").delay(300).animate( {top:"+=-3"}, 150)
+});
